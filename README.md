58799f6cf13471cf0f7c551c098d25320a987a7e
Gabriele Gatto <gabrielegatto@TP-LINK-0GHZ.local>
2016-10-13 13:58:14 +0200

    .

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # iptables_script
 Bash script to enable the firewall on startup of your Ubuntu server.
 
-Remember, to change the variables !
+@ Installation
+~:$ git clone https://github.com/garanet/iptables_script.git
+~:$ cd iptables_script/
+~:$ sudo chmod a+x *.sh
+~:$ sudo mv firewall.sh /etc/init.d/
+~:$ sudo mv iptables.sh /usr/local/bin/
+~:$ sudo update-rc.d /etc/init.d/firewall.sh defaults
+
+Remember, it's tested on Ubuntu, change the variables for your own system !
 
 2016 - www.garanet.net
---
+++ firewall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RETVAL=0
+
+# To start the firewall
+start() {
+  echo -n "Iptables rules creation: "
+  /usr/local/bin/iptables.sh
+  RETVAL=0
+}
+
+# To stop the firewall
+stop() {
+  echo -n "Removing all iptables rules: "
+  /sbin/iptables -F
+  RETVAL=0
+}
+
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  status)
+    /sbin/iptables -L
+    /sbin/iptables -t nat -L
+    RETVAL=0
+    ;;
+  *)
+    echo "Usage: firewall {start|stop|restart|status}"
+    RETVAL=1
+esac
+
+exit
+
